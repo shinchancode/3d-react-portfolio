4a89a80ba9758df5546a6749bf6cb710d9de35d5
Varun Wadhwa <79788933+varunrwadhwa@users.noreply.github.com>
2025-01-22 20:16:20 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,14 +7,15 @@ const Footer = () => {
     return (
         <div className="footer text-center">
         <p> 
-          &#x3c;&#47;&#x3e; with ❤️ by
+          &#x3c;&#47;&#x3e; with 💗 by
           <a href="https://shinchancode.github.io/3d-react-portfolio/" target="_blank">
             {" "}
-            Varun Wadhwa
+            Varun Wadhwa 
+            {" "} 
           </a>
-          😎
+          💯
         </p>
-        <p className="pink-text-gradient">No. of Visitors | <img className="visitcounter" src="https://hitwebcounter.com/counter/counter.php?page=9795911&style=0025&nbdigits=5&type=page&initCount=459" title="Counter Widget" Alt="Visit counter For Websites"   border="0" /></p>
+        {/* <p className="pink-text-gradient">No. of Visitors | <img className="visitcounter" src="https://hitwebcounter.com/counter/counter.php?page=9795911&style=0025&nbdigits=5&type=page&initCount=459" title="Counter Widget" Alt="Visit counter For Websites"   border="0" /></p> */}
 
       </div>
     );

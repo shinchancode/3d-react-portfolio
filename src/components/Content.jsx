f42c5bb303fc378c4734099c1e445d2d0d17f062
shinchanncode <shinchanncode@gmail.com>
2025-06-01 19:56:09 +0530

    Changes for tooltip addition

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,51 +1,56 @@
 import { React, useState } from "react";
 import "./Content.scss";
-import { AiOutlineHome,AiOutlineFundProjectionScreen } from "react-icons/ai";
+import { AiOutlineHome, AiOutlineFundProjectionScreen } from "react-icons/ai";
 import { BiBook } from "react-icons/bi";
 import { MdMessage } from "react-icons/md";
-import {BsPersonWorkspace} from "react-icons/bs"
+import { BsPersonWorkspace } from "react-icons/bs";
 
 const Content = () => {
-    const [activeNav, setActiveNav] = useState("#");
-    return (
-      <div className="nav">        
-        <a
-          href="#"
-          onClick={() => setActiveNav("#")}
-          className={activeNav === "#" ? "active" : ""}
-        >
-          <AiOutlineHome />
-        </a>
-        <a
-          href="#education"
-          onClick={() => setActiveNav("#education")}
-          className={activeNav === "#education" ? "active" : ""}
-        >
-          <BiBook />
-        </a>
-        <a
-          href="#project"
-          onClick={() => setActiveNav("#project")}
-          className={activeNav === "#project" ? "active" : ""}
-        >
-          <AiOutlineFundProjectionScreen />
-        </a>
-        <a
-          href="#experience"
-          onClick={() => setActiveNav("#experience")}
-          className={activeNav === "#experience" ? "active" : ""}
-        >
-          <BsPersonWorkspace />
-        </a>
-        <a
-          href="#contact"
-          onClick={() => setActiveNav("#contact")}
-          className={activeNav === "#contact" ? "active" : ""}
-        >
-          <MdMessage />
-        </a>
-      </div>
-    );
-  };
+  const [activeNav, setActiveNav] = useState("#");
+  return (
+    <div className="nav">
+      <a
+        title="Home"
+        href="#"
+        onClick={() => setActiveNav("#")}
+        className={activeNav === "#" ? "active" : ""}
+      >
+        <AiOutlineHome />
+      </a>
+      <a
+        title="Education"
+        href="#education"
+        onClick={() => setActiveNav("#education")}
+        className={activeNav === "#education" ? "active" : ""}
+      >
+        <BiBook />
+      </a>
+      <a
+        title="Projects"
+        href="#project"
+        onClick={() => setActiveNav("#project")}
+        className={activeNav === "#project" ? "active" : ""}
+      >
+        <AiOutlineFundProjectionScreen />
+      </a>
+      <a
+        title="Experience"
+        href="#experience"
+        onClick={() => setActiveNav("#experience")}
+        className={activeNav === "#experience" ? "active" : ""}
+      >
+        <BsPersonWorkspace />
+      </a>
+      <a
+        title="Contact"
+        href="#contact"
+        onClick={() => setActiveNav("#contact")}
+        className={activeNav === "#contact" ? "active" : ""}
+      >
+        <MdMessage />
+      </a>
+    </div>
+  );
+};
 
-export default Content
+export default Content;
